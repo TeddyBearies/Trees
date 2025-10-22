955fd2779316f3f78fe699a5bbd0af0a2c16802a
TeddyBearies <88991223+TeddyBearies@users.noreply.github.com>
2025-10-22 12:50:05 +0300

    Update Tree_traversals.cpp

--- Tree_traversals.cpp
+++ Tree_traversals.cpp
@@ -1,34 +1,38 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
-
-struct Node {
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
 
     Node(int value) {
         data = value;
-        left = nullptr;
-        right = nullptr;
+        left = right = nullptr;
     }
 };
-
 class Tree {
 public:
+    Node* root;
+
+    Tree() {
+        root = nullptr;
+    }
+
+    // Function to insert a node
     Node* insert(Node* node, int value) {
         if (node == nullptr) {
             return new Node(value);
         }
-
-        if (value < node->data)
+        if (value < node->data) {
             node->left = insert(node->left, value);
-        else if (value > node->data)
+        } else {
             node->right = insert(node->right, value);
-
+        }
         return node;
     }
 
-    // In-order Traversal (Left, Root, Right)
+    // In-order traversal
     void inOrderTraversal(Node* node) {
         if (node != nullptr) {
             inOrderTraversal(node->left);
@@ -37,7 +41,7 @@ public:
         }
     }
 
-    // Pre-order Traversal (Root, Left, Right)
+    // Pre-order traversal
     void preOrderTraversal(Node* node) {
         if (node != nullptr) {
             cout << node->data << " ";
@@ -46,7 +50,7 @@ public:
         }
     }
 
-    // Post-order Traversal (Left, Right, Root)
+    // Post-order traversal
     void postOrderTraversal(Node* node) {
         if (node != nullptr) {
             postOrderTraversal(node->left);
@@ -54,32 +58,112 @@ public:
             cout << node->data << " ";
         }
     }
+
+        bool search(Node* node, int value) {
+            if (node->data == value) {
+                return true;
+            } else if (value < node->data) {
+                return search(node->left, value);
+            } else {
+                return search(node->right, value);
+            }
+        }
+
+        // Function to find the minimum value node
+        Node* findMin(Node* node) {
+            while (node->left != nullptr) {
+                node = node->left;
+            }
+            return node;
+        }
+
+        // Function to find the maximum value node
+        Node* findMax(Node* node) {
+            while (node->right != nullptr) {
+                node = node->right;
+            }
+            return node;
+        }
+ 
+  
+        Node* deleteNode(Node* root, int value) {
+            if (root == nullptr) {
+                return root;
+            }
+
+            if (value < root->data) {
+                root->left = deleteNode(root->left, value);
+            } else if (value > root->data) {
+                root->right = deleteNode(root->right, value);
+            } else {
+                if (root->left == nullptr) {
+                    Node* temp = root->right;
+                    delete root;
+                    return temp;
+                } else if (root->right == nullptr) {
+                    Node* temp = root->left;
+                    delete root;
+                    return temp;
+                }
+
+                Node* temp = findMin(root->right);
+                root->data = temp->data;
+                root->right = deleteNode(root->right, temp->data);
+            }
+
+            return root;
+        }
+
 };
 
+// Driver code
 int main() {
     Tree tree;
-    Node* root = nullptr;
 
     // Insert nodes
-    root = tree.insert(root, 50);
-    root = tree.insert(root, 30);
-    root = tree.insert(root, 70);
-    root = tree.insert(root, 20);
-    root = tree.insert(root, 40);
-    root = tree.insert(root, 60);
-    root = tree.insert(root, 80);
+    tree.root = tree.insert(tree.root, 50);
+    tree.insert(tree.root, 30);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 40);
+    tree.insert(tree.root, 70);
+    tree.insert(tree.root, 60);
+    tree.insert(tree.root, 80);
 
     cout << "In-order traversal: ";
-    tree.inOrderTraversal(root);
+    tree.inOrderTraversal(tree.root);
     cout << endl;
 
     cout << "Pre-order traversal: ";
-    tree.preOrderTraversal(root);
+    tree.preOrderTraversal(tree.root);
     cout << endl;
 
     cout << "Post-order traversal: ";
-    tree.postOrderTraversal(root);
+    tree.postOrderTraversal(tree.root);
+    cout << endl;
+
+    // Search for a node
+    int searchValue = 40;
+    cout << "Searching for " << searchValue << ": ";
+    if (tree.search(tree.root, searchValue)) {
+        cout << "Found!" << endl;
+    } else {
+        cout << "Not found!" << endl;
+    }
+
+    // Find minimum and maximum node
+    Node* minNode = tree.findMin(tree.root);
+    Node* maxNode = tree.findMax(tree.root);
+    cout << "Minimum value in the tree: " << minNode->data << endl;
+    cout << "Maximum value in the tree: " << maxNode->data << endl;
+
+    // Delete a node
+    tree.root = tree.deleteNode(tree.root, 40);
+    cout << "After deleting 40, In-order traversal: ";
+    tree.inOrderTraversal(tree.root);
     cout << endl;
 
     return 0;
 }
+
+
+};
